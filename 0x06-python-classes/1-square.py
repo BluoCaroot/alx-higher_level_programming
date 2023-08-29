303f99b414de378555bc7d951dfa25aaeae50fb4
BluoCaroot <assem.akl2004@gmail.com>
2023-08-29 14:11:30 -0700

    2nd

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square.
+
+    Args:
+        size (int): the size of the square
+    """
+
+    def __init__(slef, size):
+        self.__size = size
