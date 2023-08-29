6b14237ef7d3f0d00cd5f2a0d1d63122ae885bbd
BluoCaroot <assem.akl2004@gmail.com>
2023-08-29 15:19:52 -0700

    5th

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initializes a new square
+
+        Args:
+            size (int): The size of the new square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """return area of square"""
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        """get/set size of square"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
