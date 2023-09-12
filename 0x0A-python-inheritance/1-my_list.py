1ba8ee289e4e3f463a9b3f52306d9a05c96b9e92
BluoCaroot <assem.akl2004@gmail.com>
2023-09-11 17:00:09 -0700

    no test

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""defines list MyList"""
+
+
+class MyList(list):
+    """MyList"""
+    pass
+
+    def print_sorted(self):
+        """prints sorted list"""
+        print(sorted(self))
